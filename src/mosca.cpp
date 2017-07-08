f2cc4411be8a90eea30cc14478ff618c01d3e8c9
expert975 <rodrigocomputadores125@hotmail.com>
2017-07-07 21:13:21 -0300

    Add sensor testing code

--- src/mosca.cpp
+++ src/mosca.cpp
@@ -1,7 +1,19 @@
 #include <Arduino.h>
 
+int sensorPin = A7;
+
 void setup ()
-{}
+{
+  // analogReference(INTERNAL1V1);
+  Serial.begin(115200);
+  pinMode(sensorPin, INPUT);
+  pinMode(12, OUTPUT);
+  digitalWrite(12, HIGH);
+}
 
 void loop()
-{}
+{
+  int sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
+  delay(50);
+}
